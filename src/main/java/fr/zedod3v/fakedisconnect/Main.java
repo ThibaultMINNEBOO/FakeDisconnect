7f8b4e6be30f2a369c8b3ce7c2821f9cf8a602e0
ThibaultMINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-08-25 03:47:40 +0200

    feat: plugin creation

---
+++ src/main/java/fr/zedod3v/fakedisconnect/Main.java
@@ -0,0 +1,10 @@
+package fr.zedod3v.fakedisconnect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("[FakeDisconnect] : Running...");
+    }
+}
